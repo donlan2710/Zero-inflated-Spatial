9755a73f54fd14a20281aeac85a033f1667ac74a
Lan T. Tran <donlan2710@yahoo.com>
2026-06-04 19:27:34 -0500

    Add 01_vector_data.R script

---
+++ scripts/01_vector_data.R
@@ -0,0 +1,7 @@
+# Script 01: Vector Data Operations
+# Purpose: Load, inspect, and process US county shapefiles
+# Data: Census TIGER/Line Missouri county shapefile
+# Author: Lan T. Tran
+# Date: June 2026
+install.packages("sf")
+library(sf)
